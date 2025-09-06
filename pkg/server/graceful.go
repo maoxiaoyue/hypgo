f872a4b91d67316698ffaa698fd342da6df49764
Chris Orblack <chris.black4tw@gmail.com>
2025-09-06 11:06:59 +0000

    fix server.go

--- pkg/server/graceful.go
+++ pkg/server/graceful.go
@@ -8,31 +8,37 @@ import (
 	"time"
 )
 
+// ListenAndServeWithGracefulShutdown 啟動伺服器並支援優雅關閉
 func (s *Server) ListenAndServeWithGracefulShutdown() error {
-	// 啟動服務器
+	// 應用預設中間件
+	s.applyDefaultMiddlewares()
+
+	// 啟動伺服器
 	errChan := make(chan error, 1)
 	go func() {
 		errChan <- s.Start()
 	}()
 
-	// 等待中斷信號
+	// 設置信號處理
 	quit := make(chan os.Signal, 1)
 	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+	// 等待關閉信號或錯誤
 	select {
 	case err := <-errChan:
 		return err
 	case <-quit:
-		s.logger.Info("Shutting down server...")
+		s.logger.Info("Received shutdown signal")
 
-		// 設置關閉超時
+		// 優雅關閉
 		ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 		defer cancel()
 
-		// 關閉服務器
 		if err := s.Shutdown(ctx); err != nil {
 			s.logger.Emergency("Server forced to shutdown: %v", err)
 			return err
 		}
+
 		s.logger.Info("Server exited gracefully")
 	}
 
